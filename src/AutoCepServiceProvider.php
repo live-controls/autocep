9e654ae8010ff06d5826a26fdde5c61e1a0ddbaa
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-08-19 10:17:13 -0300

    Fixed localization publish path

--- src/AutoCepServiceProvider.php
+++ src/AutoCepServiceProvider.php
@@ -20,7 +20,7 @@ class AutoCepServiceProvider extends ServiceProvider
     Livewire::component('livecontrols-autocep', AutoCep::class);
   
     $this->publishes([
-      __DIR__.'/../lang' => $this->app->langPath('vendor/livecontrols/autocep'),
+      __DIR__.'/../lang' => $this->app->langPath('vendor/livecontrols-autocep'),
     ], 'livecontrols.autocep.localization');
 
     $this->publishes([
